d6ffd5f434ebeb0920dcfaabdd21c19e26f7cdbd
Thomas <techybolek@yahoo.com>
2023-05-13 09:19:17 -0500

    chuck

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
 
 function Home() {
+  const [joke, setJoke] = useState('');
+
+    const fetchJoke = async () => {
+    try {
+      const response = await fetch('https://api.chucknorris.io/jokes/random');
+      const data = await response.json();
+      setJoke(data.value);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchJoke();
+
+    const interval = setInterval(fetchJoke, 30000); // Fetch a new joke every 30 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleRefresh = () => fetchJoke()
+
   return (
-    <div className="home">
+    <div className="home" onClick={handleRefresh}>
       <h2>Welcome to My Portfolio</h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod risus ac sem rutrum, vel ullamcorper ipsum
@@ -15,6 +37,10 @@ function Home() {
         amet diam hendrerit ullamcorper. Donec tincidunt mauris id aliquam congue. Ut ut urna vel ipsum ultrices
         venenatis. Integer rhoncus aliquet lacinia.
       </p>
+      <div className="chuck-norris-joke">
+        <h3>Chuck Norris Joke of the Moment</h3>
+        <p>{joke}</p>
+      </div>
       <button className="cta-button">Contact Me</button>
     </div>
   );
